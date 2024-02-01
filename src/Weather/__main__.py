156e68dd0cd05346f74a3d1477ebad72d87fd693
Tyler Kaus <tylerkaus@Tylers-MacBook-Pro-2.local>
2024-02-01 11:47:51 -0600

    new changes Feb. 1

--- src/Weather/__main__.py
+++ src/Weather/__main__.py
@@ -1,13 +1,27 @@
-import argparse
-import requests 
-import json
+#import argparse
+#import requests 
+#import json
 import sys
+
+#importinClasses
+sys.path.append("..")
+from fileManipulator import FileManipulator
 
 n = len(sys.argv)
 
 if (n == 1):
     print("Too few arguments")
 else: 
-    if argv[1] == "setup":
+    if sys.argv[1] == "setup":
         print('Program is working')
-        
+    elif sys.argv[1] == "update":
+        FileManipulator.update() 
+    elif sys.argv[1] == "current":
+        output = FileManipulator.current()
+        print(output, "F")
+    elif sys.argv[1] == 'help':
+        print("""
+              Commands: 
+                setup
+                update
+                current""")
--- src/fileManipulator.py
+++ src/fileManipulator.py
@@ -1,15 +1,19 @@
-import argparse
+#import argparse
 import requests 
 import json
-response = requests.get("https://api.weather.gov/gridpoints/FSD/149,22/forecast")
 
-def jprint(obj):
-    # create a formatted string of the Python JSON object
-    text = json.dumps(obj, sort_keys=True, indent=4)
-    print(text)
 
-data = response.json()
+class FileManipulator():
+    def update():
+        response = requests.get("https://api.weather.gov/gridpoints/FSD/149,22/forecast")
+        data = response.json()
+        with open("data.json","w+") as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
+        return
 
-with open("text.json", "w+") as f:
-    json.dump(data, f, ensure_ascii=False, indent=4)
+    def current():
+        with open('data.json', 'r') as file:
+            weatherData = json.load(file)
+        output = weatherData['properties']['periods'][0]['temperature']
+        return output
 
